0eac1074f511b588ff5ab27dcc849b03c9ecc19d
Samael Picoli <samaelmunizpicoli27@gmail.com>
2024-09-27 17:57:58 -0300

    1.42 - ajustes nos arquivos css e js

---
+++ core/static/js/exclusao_item.js
@@ -0,0 +1,16 @@
+// Script para remover mensagens de feedback após 2 segundos
+window.setTimeout(function(){
+    $('.alert').fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove();
+    });
+}, 2000);
+
+// Script para mostrar o modal e configurar o botão de apagar com a URL correta
+$('#deleteModal').on('show.bs.modal', function (event) {
+    // Obtém o botão que acionou o modal e a URL
+    var button = $(event.relatedTarget) 
+    var url = button.data('url') 
+    var confirmBtn = $('#confirmDeleteBtn')
+    // Atualiza o href do botão de confirmação com a URL do item a ser deletado
+    confirmBtn.attr('href', url)
+});
---
+++ staticfiles/js/formulario_venda/input_formulario.js
@@ -0,0 +1,32 @@
+// Leitura do documento
+$(document).ready(function() {
+
+    // Inicializar select2
+    $('#id_cliente').select2();
+    $('#id_produto').select2();
+    $('#id_vendedor').select2();
+
+    
+    // Função para garantir que os valores sejam positivos
+    function positiveValue(input) {
+        if (input.value < 0) {
+            input.value = Math.abs(input.value);
+        }
+    }
+
+
+    $('#quantidade, #valor_unitario, #id_nota_fiscal').on('input', function() {
+        positiveValue(this);
+        calcularTotal();
+    });
+});
+
+
+// Função para calcular o valor total automaticamente
+function calcularTotal() {
+    var quantidade = document.getElementById("quantidade").value;
+    var valorUnitario = document.getElementById("valor_unitario").value; 
+    var valorTotal = quantidade * valorUnitario;
+
+    document.getElementById("valor_total").value = valorTotal.toFixed(2);
+}
