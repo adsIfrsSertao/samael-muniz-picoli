e4464ce3fef71f0187b605f9ffeb9e1707bb2bb5
Samael Picoli <samaelmunizpicoli27@gmail.com>
2024-12-26 21:48:12 -0300

    fixing a bug

--- projeto/settings.py
+++ projeto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['evo-agricola-bi-2bddb7a7baf0.herokuapp.com']
+ALLOWED_HOSTS = ['evoagricolabi-10edde98d839.herokuapp.com']
 
 
 # Application definition
